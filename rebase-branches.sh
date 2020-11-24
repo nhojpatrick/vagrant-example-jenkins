652f0b2e59e87295d94b9c5bde1be2afe2585fbb
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2020-11-24 20:28:30 +0000

    add v2.263.x

--- rebase-branches.sh
+++ rebase-branches.sh
@@ -4,4 +4,5 @@ git checkout v2.204.x && git rebase main     && git push -f && \
 git checkout v2.222.x && git rebase v2.204.x && git push -f && \
 git checkout v2.235.x && git rebase v2.222.x && git push -f && \
 git checkout v2.249.x && git rebase v2.235.x && git push -f && \
-git checkout v2.x     && git rebase v2.249.x && git push -f
+git checkout v2.263.x && git rebase v2.249.x && git push -f && \
+git checkout v2.x     && git rebase v2.263.x && git push -f
