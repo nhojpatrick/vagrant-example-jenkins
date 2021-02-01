a5288175367716eb2f0c46a11f6d3c293f404688
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2021-02-01 21:20:12 +0000

    older LTS versions

--- rebase-branches.sh
+++ rebase-branches.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-git checkout v2.204.x && git rebase main     && git push -f && \
+git checkout v2.176.x && git rebase main     && git push -f && \
+git checkout v2.190.x && git rebase v2.176.x && git push -f && \
+git checkout v2.204.x && git rebase v2.190.x && git push -f && \
 git checkout v2.222.x && git rebase v2.204.x && git push -f && \
 git checkout v2.235.x && git rebase v2.222.x && git push -f && \
 git checkout v2.249.x && git rebase v2.235.x && git push -f && \
 git checkout v2.263.x && git rebase v2.249.x && git push -f && \
-git checkout v2.x     && git rebase v2.263.x && git push -f
+git checkout v2.x     && git rebase v2.263.x && git push -f && \
+git checkout main ;
+
