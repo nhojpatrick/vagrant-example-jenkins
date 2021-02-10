1405b96ea9bc70a00c04686ea5bf574755136976
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2021-02-10 21:46:33 +0000

    end on latest LTS branch

--- rebase-branches.sh
+++ rebase-branches.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+git checkout main     && git pull            && git push -f && \
 git checkout v2.176.x && git rebase main     && git push -f && \
 git checkout v2.190.x && git rebase v2.176.x && git push -f && \
 git checkout v2.204.x && git rebase v2.190.x && git push -f && \
@@ -8,5 +9,5 @@ git checkout v2.235.x && git rebase v2.222.x && git push -f && \
 git checkout v2.249.x && git rebase v2.235.x && git push -f && \
 git checkout v2.263.x && git rebase v2.249.x && git push -f && \
 git checkout v2.x     && git rebase v2.263.x && git push -f && \
-git checkout main ;
+git checkout v2.263.x ;
 
