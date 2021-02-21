e1d2890bff345601be07a80a9713b00cc7b5f28e
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2021-02-21 11:48:47 +0000

    add LTS branch v2.277.x

--- rebase-branches.sh
+++ rebase-branches.sh
@@ -8,5 +8,6 @@ git checkout v2.222.x && git rebase v2.204.x && git push -f && \
 git checkout v2.235.x && git rebase v2.222.x && git push -f && \
 git checkout v2.249.x && git rebase v2.235.x && git push -f && \
 git checkout v2.263.x && git rebase v2.249.x && git push -f && \
-git checkout v2.x     && git rebase v2.263.x && git push -f && \
+git checkout v2.277.x && git rebase v2.263.x && git push -f && \
+git checkout v2.x     && git rebase v2.277.x && git push -f && \
 git checkout v2.263.x ;
